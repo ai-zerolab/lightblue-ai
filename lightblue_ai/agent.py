cb2ea5f62417c8d5df48f20a8c4c3d00bcfcad5f
Wh1isper <9573586@qq.com>
2025-04-02 23:08:08 +0800

    allow extra tools and mcp servers

--- lightblue_ai/agent.py
+++ lightblue_ai/agent.py
@@ -3,6 +3,8 @@ from typing import TypeVar
 
 from pydantic_ai.agent import Agent, AgentRunResult
 from pydantic_ai.messages import UserContent
+from pydantic_ai.tool import Tool
+from pydantic_ai.mcp import MCPServer
 
 from lightblue_ai.mcps import get_mcp_servers
 from lightblue_ai.models import infer_model
@@ -16,21 +18,26 @@ T = TypeVar("T")
 class LightBlueAgent[T]:
     def __init__(
         self,
+        system_prompt: str | None = None,
         result_type: T = str,
         result_tool_name: str = "final_result",
         result_tool_description: str | None = None,
+        tools: list[Tool] | None = None,
+        mcp_servers: list[MCPServer] | None = None,
     ):
         self.tool_manager = LightBlueToolManager()
         self.settings = Settings()
+        tools = tools or []
+        mcp_servers = mcp_servers or []
 
         self.agent = Agent(
             infer_model(self.settings.default_model),
             result_type=result_type,
             result_tool_name=result_tool_name,
             result_tool_description=result_tool_description,
-            system_prompt=get_system_prompt(),
-            tools=self.tool_manager.get_all_tools(),
-            mcp_servers=get_mcp_servers(),
+            system_prompt=system_prompt or get_system_prompt(),
+            tools=[*tools, *self.tool_manager.get_all_tools()],
+            mcp_servers=[*mcp_servers, *get_mcp_servers()],
         )
 
     async def run(self, user_prompt: str | Sequence[UserContent]) -> AgentRunResult[T]:
