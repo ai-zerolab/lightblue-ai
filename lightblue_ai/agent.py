9f2a03eb39ec8d9bfbc467f8cd2bc4cfb6845279
Zhongsheng Ji <9573586@qq.com>
2025-04-08 16:40:29 +0800

    feat: Support stream (#8)
    * feat: Support stream
    
    * Implement stream with rich
    
    * Support stream as time

--- lightblue_ai/agent.py
+++ lightblue_ai/agent.py
@@ -1,11 +1,19 @@
 from collections.abc import AsyncIterator, Sequence
+from contextlib import asynccontextmanager
 from typing import TypeVar
 
 from pydantic_ai.agent import Agent, AgentRun, AgentRunResult
 from pydantic_ai.mcp import MCPServer
-from pydantic_ai.messages import UserContent
+from pydantic_ai.messages import (
+    AgentStreamEvent,
+    FinalResultEvent,
+    HandleResponseEvent,
+    ModelMessage,
+    UserContent,
+)
 from pydantic_ai.models import Model
 from pydantic_ai.tools import Tool
+from pydantic_ai.usage import Usage
 
 from lightblue_ai.log import logger
 from lightblue_ai.mcps import get_mcp_servers
@@ -46,12 +54,50 @@ class LightBlueAgent[T]:
             mcp_servers=[*mcp_servers, *get_mcp_servers()],
         )
 
-    async def run(self, user_prompt: str | Sequence[UserContent]) -> AgentRunResult[T]:
+    async def run(
+        self,
+        user_prompt: str | Sequence[UserContent],
+        *,
+        message_history: None | list[ModelMessage] = None,
+        usage: None | Usage = None,
+    ) -> AgentRunResult[T]:
         async with self.agent.run_mcp_servers():
-            return await self.agent.run(user_prompt)
+            result = await self.agent.run(user_prompt, message_history=message_history)
+        if usage:
+            usage.incr(result.usage(), requests=1)
 
-    async def iter(self, user_prompt: str | Sequence[UserContent]) -> AsyncIterator[AgentRun]:
-        async with self.agent.run_mcp_servers(), self.agent.iter(user_prompt) as run:
-            async for node in run:
-                yield node
-        logger.info(f"{run.usage()}")
+        return result
+
+    @asynccontextmanager
+    async def iter(
+        self,
+        user_prompt: str | Sequence[UserContent],
+        *,
+        message_history: None | list[ModelMessage] = None,
+        usage: None | Usage = None,
+    ) -> AsyncIterator[AgentRun]:
+        async with (
+            self.agent.run_mcp_servers(),
+            self.agent.iter(user_prompt, message_history=message_history) as run,
+        ):
+            yield run
+
+        if usage:
+            usage.incr(run.usage(), requests=1)
+
+    async def yield_response_event(self, run: AgentRun) -> AsyncIterator[HandleResponseEvent | AgentStreamEvent]:
+        """
+        Yield the response event from the node.
+        """
+        async for node in run:
+            if Agent.is_user_prompt_node(node) or Agent.is_end_node(node):
+                continue
+
+            elif Agent.is_model_request_node(node) or Agent.is_call_tools_node(node):
+                async with node.stream(run.ctx) as request_stream:
+                    async for event in request_stream:
+                        if not event or isinstance(event, FinalResultEvent):
+                            continue
+                        yield event
+            else:
+                logger.warning(f"Unknown node: {node}")
